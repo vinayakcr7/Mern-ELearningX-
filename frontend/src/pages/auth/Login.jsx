11f387f9db8a9f3722e831e8e81e161a4a7548ca
Vinayak Mahyavanshi <131753435+vinayakcr7@users.noreply.github.com>
2025-12-24 16:35:28 +0530

    Fix typo in password input required attribute

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -25,7 +25,7 @@ import { CourseData } from "../../context/CourseContext";
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
           <label htmlFor="password">Password</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} requiredrequired />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
           <button disabled={btnloading} type="submit" className="common-btn">
             {btnloading?"Please Wait...": "Login"}
